20b04966a74e39f5b6f14294f33c16cbecd00d85
Srinivas <srinivasvinnakota799@gmail.com>
2026-02-21 11:04:42 +0530

    Update 1_swap.c

--- Functions/1_swap.c
+++ Functions/1_swap.c
@@ -17,4 +17,21 @@ void swap(int x, int y)
    a=b;
    b=temp;
 }
+// for refrence
+
+
+#include<stdio.h>
+int swap(int *x,int *y){
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+int main(){
+    int a=10,b=20;
+    swap(&a,&b);
+     printf("%d %d",a,b);
+    return 0;
+}
 
